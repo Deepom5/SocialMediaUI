2db3bab309a12c2240c155dd30c658bf284b8dbc
Deepom-A <87470912+Deepom-A@users.noreply.github.com>
2023-06-18 22:48:49 +0530

    commit

---
+++ screens/Notifications.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Notifications = () => {
+    return (
+        <View style={{justifyContent:"center", alignContent:"center", }} >
+            <Text style={{marginTop:400,marginLeft:160 }}>Notifications</Text>
+        </View>
+    )
+}
+
+export default Notifications
